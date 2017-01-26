03c26700c055965ab59110a7ee319b264d83e433
Ang Cheng Siew <johnacsyen@gmail.com>
2017-01-26 08:30:41 +0800

    added background image, added hour hand in-between

--- clock.js
+++ clock.js
@@ -1,66 +1,61 @@
 
+function clock () {
+  var secondsElement = document.getElementById('second')
+  var minutesElement = document.getElementById('minute')
+  var hoursElement = document.getElementById('hour')
 
+  var now = new Date()
+  var timeInSeconds = now.getSeconds()
+  var timeInMinutes = now.getMinutes()
+  var timeInHours = now.getHours()
 
-function clock(){
-var secondsElement = document.getElementById('second')
-var minutesElement = document.getElementById('minute')
-var hoursElement = document.getElementById('hour')
-
-var now = new Date()
-var timeInSeconds = now.getSeconds()
-var timeInMinutes = now.getMinutes()
-var timeInHours = now.getHours()
-
-function init () {
-  setInterval(displayTime, 1000)
-}
-
-function displayTime () {
-  now = new Date()
-  timeInSeconds = now.getSeconds()
-  timeInMinutes = now.getMinutes()
-  timeInHours = now.getHours()
-  // console.log(now)
-  secondsElement.style.transform = 'rotate(' + secondRotation(timeInSeconds) + 'deg)'
-  minutesElement.style.transform = 'rotate(' + minuteRotation(timeInMinutes) + 'deg)'
-  hoursElement.style.transform = 'rotate(' + hourRotation(timeInHours) + 'deg)'
-}
-
-function secondRotation (mySeconds) {
-  var angle = mySeconds * 6
-
-  return angle
-}
-
-function minuteRotation (myMinute) {
-  var angle = myMinute * 6
-
-  return angle
-}
-
-function hourRotation (myHour) {
-  myHour = myHour % 12
-  if (myHour === 0) {
-    return 0
-  } else {
-    var hourPlusFraction = (myHour / 12) * 360
-    return hourPlusFraction
+  function init () {
+    setInterval(displayTime, 1000)
   }
-}
 
-return{
-  init: init
+  function displayTime () {
+    now = new Date()
+    timeInSeconds = now.getSeconds()
+    timeInMinutes = now.getMinutes()
+    timeInHours = now.getHours()
+  // console.log(now)
+    secondsElement.style.transform = 'rotate(' + secondRotation(timeInSeconds) + 'deg)'
+    minutesElement.style.transform = 'rotate(' + minuteRotation(timeInMinutes) + 'deg)'
+    hoursElement.style.transform = 'rotate(' + hourRotation(timeInHours) + 'deg)'
+  }
+
+  function secondRotation (mySeconds) {
+    var angle = mySeconds * 6
+
+    return angle
+  }
+
+  function minuteRotation (myMinute) {
+    var angle = myMinute * 6
+
+    return angle
+  }
+
+  function hourRotation (myHour) {
+    myHour = myHour % 12
+    if (myHour === 0) {
+      return 0
+    } else {
+      var hourPlusFraction = (myHour / 12) * 360 + (timeInMinutes * 0.5)
+      return hourPlusFraction
+    }
+  }
+
+  return {
+    init: init
   // displayTime: displayTime,
   // secondRotation:secondRotation,
   // minuteRotation: minuteRotation,
   // hourRotation: hourRotation
+  }
 }
 
-}
-
-
-document.addEventListener('DOMContentLoaded', function(){
-var myClock = clock()
-myClock.init()
-
+document.addEventListener('DOMContentLoaded', function () {
+  var myClock = clock()
+  myClock.init()
 })
